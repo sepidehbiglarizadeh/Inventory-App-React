82ff78b951aec3c0dd42af18bfc42566e988b9ce
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-09 18:45:21 +0330

    delete product and show category name in productList

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ const App = () => {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
 
+  const deleteProductHandler = (productId) => {
+    const filteredProducts = products.filter((p) => p.id !== productId);
+    setProducts(filteredProducts);
+  };
 
   return (
     <>
       <div className="bg-slate-800 min-h-screen">
         <NavBar />
         <div className="container max-w-screen-sm mx-auto p-2">
-            <CategoryForm setCategories={setCategories}/>
-            <ProductsForm categories={categories} setProducts={setProducts}/>
-            <ProductList products={products} />
+          <CategoryForm setCategories={setCategories} />
+          <ProductsForm categories={categories} setProducts={setProducts} />
+          <ProductList
+            products={products}
+            deleteProductHandler={deleteProductHandler}
+            categories={categories}
+          />
         </div>
       </div>
     </>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,23 +1,29 @@
-const ProductList = ({ products }) => {
+const ProductList = ({ products,deleteProductHandler ,categories}) => {
+    const findCategory=(id)=>{
+       return categories.find(c => c.id === parseInt(id)).title
+    }
+
   return (
     <>
       <h2 className="text-xl text-slate-300 font-bold mb-2">ProductList</h2>
+      <div className="overflow-x-auto">
       {products.map((product) => {
         return (
-          <div class="flex items-center justify-between mb-2">
-            <span class="text-slate-400">{product.title}</span>
-            <div class="flex items-center gap-x-3">
-              <span class="text-slate-400">
+          <div key={product.id} className="flex items-center justify-between mb-2">
+            <span className="text-slate-400">{product.title}</span>
+            <div className="flex items-center gap-x-3">
+              <span className="text-slate-400">
                 {new Date(product.createdAt).toLocaleString("fa-IR")}
               </span>
-              <span class="block px-3 py-0.5 text-slate-400 text-sm border border-slate-400 rounded-2xl">
-                {product.categoryId}
+              <span className="block px-3 py-0.5 text-slate-400 text-sm border border-slate-400 rounded-2xl">
+                {findCategory(product.categoryId)}
               </span>
-              <span class="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 border-2 border-slate-300 text-slate-300">
+              <span className="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 border-2 border-slate-300 text-slate-300">
                 {product.quantity}
               </span>
               <button
-                class="delete-product border px-2 py-0.5 rounded-2xl border-red-400 text-red-400"
+                className="delete-product border px-2 py-0.5 rounded-2xl border-red-400 text-red-400"
+                onClick={()=>deleteProductHandler(product.id)}
               >
                 Delete
               </button>
@@ -25,6 +31,7 @@ const ProductList = ({ products }) => {
           </div>
         );
       })}
+      </div>
     </>
   );
 };
